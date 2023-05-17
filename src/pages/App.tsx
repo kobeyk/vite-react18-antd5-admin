fc1603f181bbc636e56d9bd3de18ea1a0225b381
appleyk <yukun24@126.com>
2023-05-17 22:55:28 +0800

    主题色配置

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,18 @@
-import { Button, message } from 'antd';
+import { Button, Radio, Checkbox, message } from 'antd';
 import { FC, ReactElement } from 'react';
-const App:FC = ():ReactElement => {
-  const _onClick=() => {
+const App: FC = (): ReactElement => {
+  const _onClick = () => {
     message.success("success")
   }
   return (
     <div>
       <h1>
         <Button type="primary" onClick={_onClick}>登录</Button>
+        <hr />
+        <Radio defaultChecked={true}>Radio</Radio>
+        <Checkbox>Checkbox</Checkbox>
+        <hr />
+
       </h1>
     </div>
   )
