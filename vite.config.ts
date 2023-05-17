71a6650d72300a9a7b119f4d65b86634a46a97f8
appleyk <yukun24@126.com>
2023-05-17 22:56:21 +0800

    主题色配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path' // 消除node错误提示，需要安装 npm i -D @types/node
-// import { visualizer } from 'rollup-plugin-visualizer' //查看生成stats打包视图
-import styleImport from 'vite-plugin-style-import'
+import { visualizer } from 'rollup-plugin-visualizer' //查看生成stats打包视图
+import {
+  createStyleImportPlugin,
+  AndDesignVueResolve,
+  VantResolve,
+  ElementPlusResolve,
+  NutuiResolve,
+  AntdResolve,
+} from 'vite-plugin-style-import'
 import lessToJS from 'less-vars-to-js'
 import fs from 'fs'
 
 const themeVariables = lessToJS(
-	fs.readFileSync(path.resolve(__dirname, './src/style/theme.less'), 'utf8')
+  fs.readFileSync(path.resolve(__dirname, './src/style/theme.less'), 'utf8')
 )
 
 console.log(themeVariables)
@@ -16,51 +23,58 @@ console.log(themeVariables)
 export default defineConfig({
   plugins: [
     react(),
-    // visualizer({
-    //   open:true,
-    //   gzipSize:true,
-    //   brotliSize:true
-    // }),
-    styleImport({
-			libs: [
-				{
-					libraryName: 'antd',
-					esModule: true,
-					resolveStyle: name => {
-						return `antd/es/${name}/style/index`
-					},
-				},
-			],
-		}),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true
+    }),
+    createStyleImportPlugin({
+      resolves: [
+        AndDesignVueResolve(),
+        VantResolve(),
+        ElementPlusResolve(),
+        NutuiResolve(),
+        AntdResolve(),
+      ],
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: name => {
+            if (name !== 'theme') {
+              return `antd/es/${name}/style/index`
+            }
+          },
+        },
+      ],
+    }),
   ],
-  resolve:{
+  resolve: {
     // 配置别名
-    alias:{
-      '@':path.resolve(__dirname,'src')
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     },
-    extensions:['.js','.jsx', '.ts', '.tsx', '.json'] //导入时想要忽略的扩展名列表
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'] //导入时想要忽略的扩展名列表
   },
-  css:{
+  css: {
     modules: {
       generateScopedName: '[name]__[local]___[hash:base64:5]',
       hashPrefix: 'prefix'
     },
-    preprocessorOptions:{
+    preprocessorOptions: {
       less: {
-        javascriptEnabled: true,
-        modifyVars:themeVariables,
-        additionalData: `@import "${path.resolve(__dirname, 'src/style/global.less')}";`,
+        // 由于antd5摒弃了less，完全采用了css in js，所以网上的配置这个达到修改antd ui主色的是走不通的
       }
     }
   },
-  server:{
+  server: {
     // 服务端口
-    port:3020,
+    port: 3020,
     // 主机 解决vite use--host to expose
-    host:'0.0.0.0',
+    host: '0.0.0.0',
     // 自动打开浏览器
-    open:false,
-    cors:true
+    open: false,
+    cors: true
     // https: false,
     // 代理跨域（mock 不需要配置，这里只是个事列）
     // proxy: {
