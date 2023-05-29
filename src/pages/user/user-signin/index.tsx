beefe3b544b262fe04e720fa6d8445d47f81626e
appleyk <yukun24@126.com>
2023-05-29 22:43:06 +0800

    登录路由页样式调整

--- src/pages/user/user-signin/index.tsx
+++ src/pages/user/user-signin/index.tsx
@@ -2,15 +2,15 @@ import "../index.scss";
 const UserSignIn = () => {
   return (
     <div className="user-session-wrapper">
-        <div className="wrapper-container">
-            <div className="container-body">
-                <div className="session-left-box"></div>
-                <div className="session-right-form"></div>
-            </div>
-            <div className="container-footer">
-
-            </div>
+      <div className="wrapper-container">
+        <div className="container-body">
+          <div className="session-left-box">session-left-box</div>
+          <div className="session-right-form">session-right-form</div>
         </div>
+        <div className="container-footer">
+          container-footer
+        </div>
+      </div>
     </div>
   )
 }
