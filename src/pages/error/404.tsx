b587331056610fc0c416215ce6cda378140561e2
appleyk <appleyk211>
2023-07-13 14:16:52 +0800

    补充404页面回退事件

--- src/pages/error/404.tsx
+++ src/pages/error/404.tsx
@@ -1,12 +1,17 @@
 import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate("/home");
+  };
   return (
     <Result
       status="404"
       title="404"
       subTitle="Sorry, the page you visited does not exist."
-      extra={<Button type="primary">Back Home</Button>}
+      extra={<Button type="primary"  onClick={goHome}>Back Home</Button>}
     />
   );
 };
